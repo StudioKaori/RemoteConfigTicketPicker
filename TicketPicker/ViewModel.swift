77a2fff08e403dd8f7489bff376ef527665a399a
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-17 15:40:27 +0200

    Update: setupRemoteConfigDefaults

--- TicketPicker/ViewModel.swift
+++ TicketPicker/ViewModel.swift
@@ -11,16 +11,39 @@ import Firebase
 class ViewModel: ObservableObject {
 	@Published var textLabel = "Default"
 	
+	private let remoteConfig = RemoteConfig.remoteConfig()
+	private let remoteConfigSettings = RemoteConfigSettings()
+	
+	
 	func setupRemoteConfigDefaults() {
+		// set remote config
+		self.remoteConfigSettings.minimumFetchInterval = 0
+		self.remoteConfig.configSettings = remoteConfigSettings
+		
+		// setup default value
 		let defaultValues = [
 			"textLabel": "Default text" as NSObject,
 			"textLabel2": "Default text2" as NSObject
 		]
 		
-		RemoteConfig.remoteConfig().setDefaults(defaultValues)
+		remoteConfig.setDefaults(defaultValues)
 	}
 	
 	func fetchRemoteConfig() {
 		
+		let remoteConfigSettings = RemoteConfigSettings()
+		// FIXME: remove this before we go into production, debug settings
+		remoteConfigSettings.minimumFetchInterval = 0
+		
+		RemoteConfig.remoteConfig().fetch(withExpirationDuration: 0) { (status, error) in
+			guard error == nil else {
+				print("Got an error fetching remote values: \(error)")
+				return
+			}
+			
+			print("Yeah! Received values from the cloud!")
+			RemoteConfig.remoteConfig().
+			
+		}
 	}
 }
