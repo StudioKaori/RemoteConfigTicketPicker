78379d4b430c9383d4f7a766bf684aa2c35b04ed
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-17 14:48:08 +0200

    Initial Commit

---
+++ TicketPicker/TicketPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketPickerApp.swift
+//  TicketPicker
+//
+//  Created by Kaori Persson on 2022-10-17.
+//
+
+import SwiftUI
+
+@main
+struct TicketPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
